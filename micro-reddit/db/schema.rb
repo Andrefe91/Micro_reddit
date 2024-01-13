d6098ff2e4be72e00bcc322ec7c34c90e2a560a0
Andres Felipe -Main <andrefe91@gmail.com>
2024-01-13 12:16:10 -0500

    Migration for post and schema changes

--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_12_040838) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_165223) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_12_040838) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
